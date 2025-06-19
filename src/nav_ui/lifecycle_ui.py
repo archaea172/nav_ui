65143cb1cba9cefda52d0798f4bdaccd934c7265
aky <akiyugata271@gmail.com>
2025-06-19 22:04:46 +0900

    必要なものを追加

--- src/nav_ui/lifecycle_ui.py
+++ src/nav_ui/lifecycle_ui.py
@@ -1,3 +1,12 @@
 import rclpy
 from rclpy.node import Node
 
+from lifecycle_msgs.msg import State
+from lifecycle_msgs.msg import Transition
+
+from lifecycle_msgs.srv import ChangeState
+from lifecycle_msgs.srv import GetState
+
+class ControlLifecycle(Node):
+    def __init__(self):
+        super().__init__('control_lifecycle')
